a316eb44505e4e7574c8a3d6e71e8b530c6a04ec
Yulan Lin <yulan.lin.2014@gmail.com>
2019-03-16 23:07:02 -0700

    edits

---
+++ Scratchpad.md
@@ -0,0 +1,11 @@
+# Scratchpad
+
+## What's the relationship between size_t and number of characters that fit in a string?
+
+## Day 1
+Pseudocode
+- parse string into array of numbers (allocate half the "buffer size" to a signed int array)
+- create a function for reallocating
+
+Helper function:
+
--- test.c
+++ test.c
@@ -4,54 +4,87 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool inarray(double val, double arr[], int size)
+typedef struct DOUBLE_ARR {
+    int count;
+    long* arr;
+} DOUBLE_ARR;
+
+bool inarray(long val, long arr[], int size)
 {
     for (int i = 0; i < size; i++)
     {
         if (arr[i] == val)
         {
-            printf("Val: %f\n", val);
+            printf("Val: %ld\n", val);
             return true;
         }
     }
     return false;
 }
+// Converts the string into an array of signed ints
+struct DOUBLE_ARR numArray(char *input, int input_size)
+{
+    // each line is at least three characters: sign, int, newline
+    long* arr = malloc (input_size * sizeof(long));
+    long* arr_ind = arr;
+    int count = 0;
+
+    char* token;
+    char s[2] = "\n";
+    char* ptr;
+    long res;
+    token = strtok(input, s);
+    while (token != NULL) {
+        res = strtol(token, &ptr, 10);
+        *arr_ind = res;
+        token = strtok(NULL, s);
+        arr_ind++;
+        count ++;
+    }
+        
+    DOUBLE_ARR returnArr = {count, arr};
+    return returnArr;
+}
 
 static inline long part2(char *input, int input_size)
 {
-    double seen[10];
-    double *seen_ind = seen;
+  	// Start with 64k, which should cover most inputs without reallocating
+	size_t allocated = 65536;
+	long* seen_buff = malloc(allocated);
+    long* seen_ind = seen_buff;
+	size_t seen_size = 0;
 
+    DOUBLE_ARR array = numArray(input, input_size); 
+    long* arr = array.arr;
+    int item_count = array.count;
+
+    
+    bool already_seen = false;
+
+	// Read stdin, reallocating as necessary, until we reach EOF
+    int item_ind = 0;
     long sum = 0;
-    char *token;
-    char s[2] = "\n";
-    char *ptr;
-    long res;
-
-    char copy[input_size];
-    strcpy(copy, input);
-
-    while (true)
-    {
-
-        token = strtok(copy, s);
-        while (token != NULL)
-        {
-            *seen_ind = sum;
-            res = strtol(token, &ptr, 10);
-            printf("%ld\n", sum);
-            sum += res;
-            if (inarray(sum, seen, seen_ind - seen))
-            {
-                return sum;
-                break;
-            }
-            token = strtok(NULL, s);
+	while(!already_seen) {
+        *seen_ind = sum;
+        seen_ind++;
+        
+        // make wrap around the index
+        if (item_ind == item_count){
+            item_ind = 0;
         }
-        strcpy(copy, input);
+        sum += arr[item_ind];
+        item_ind++;
+        already_seen = inarray(sum, seen_buff, seen_size);
 
-    }
+		// If necessary, reallocate the buffer. See https://en.wikipedia.org/wiki/Dynamic_array#Growth_factor
+		// for details of why we multiply by 3/2.
+		if((seen_buff + 1) >= allocated) {
+			allocated = (allocated * 3) / 2;
+			seen_buff = realloc(seen_buff, allocated);
+		}
 
+		seen_size += 1;
+	}
     return sum;
 }
 
