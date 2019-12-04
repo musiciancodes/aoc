e1165e3822390335acfcfd63c1ac40805d941adb
Yulan Lin <yulan.lin.2014@gmail.com>
2019-12-03 23:51:38 -0800

    part 2!

--- 2019_04/src/lib.rs
+++ 2019_04/src/lib.rs
@@ -15,6 +15,21 @@ fn third() {
     assert_eq!(check_criteria(123789), false);
 }
 
+#[test]
+fn t2_1() {
+    assert_eq!(check_criteria_2(112233), true);
+}
+#[test]
+
+fn t2_2() {
+    assert_eq!(check_criteria_2(123444), false);
+}
+#[test]
+
+fn t2_3() {
+    assert_eq!(check_criteria_2(111122), true);
+}
+
 pub fn check_criteria(i: i32) -> bool {
     let i_str = i.to_string();
     let mut i: i32 = 0;
@@ -40,6 +55,47 @@ pub fn check_criteria(i: i32) -> bool {
     }
     return false;
 }
+
+pub fn check_criteria_2(i: i32) -> bool {
+    let i_str = i.to_string();
+    let mut i: i32 = 0;
+    for c in i_str.chars() {
+        let x:i32 = c.to_digit(10).unwrap().try_into().unwrap();
+        if x < i {
+            return false;
+        }
+        else {
+            i = x;
+        }
+    }
+
+    let mut adj:i32 = 1;
+    let mut adj_count:i32 = 0;
+    for c in i_str.chars() {
+        let x:i32 = c.to_digit(10).unwrap().try_into().unwrap();
+        if x == adj {
+            adj_count += 1;
+        }
+        else {
+            if adj_count == 2 {return true};
+            adj_count = 1;
+            adj = x;
+        }
+    }
+    if adj_count == 2 {return true};
+    return false;
+}
+
+pub fn part_2 (s: &str) -> i32 {
+    let range: Vec<i32> = s.split("-").map(|s| s.parse().unwrap()).collect();
+    let mut count = 0;
+    for x in range[0]..range[1]{
+        if check_criteria_2(x){count += 1}
+    }
+    return count;
+
+}
+
 pub fn part_1 (s: &str) -> i32 {
     let range: Vec<i32> = s.split("-").map(|s| s.parse().unwrap()).collect();
     let mut count = 0;
--- 2019_04/src/main.rs
+++ 2019_04/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{Read};
-use aoc::{part_1};
+use aoc::{part_1, part_2};
 
 
 fn main() {
@@ -8,6 +8,9 @@ fn main() {
     let mut input = File::open(path).unwrap();
     let mut buffer = String::new();
     input.read_to_string(&mut buffer).unwrap();
-    let solution = part_1(&buffer);
+    let mut solution = part_1(&buffer);
     println!("Part 1: {}", solution);    
+    input.read_to_string(&mut buffer).unwrap();
+    solution = part_2(&buffer);
+    println!("Part 2: {}", solution);        
 }
