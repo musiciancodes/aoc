388716bbd3ee4ebf9d93e5dd7fe54fc82b3a9139
Yulan Lin <yulan.lin.2014@gmail.com>
2019-12-06 00:23:00 -0800

    wow part 1

--- 2019_05/src/lib.rs
+++ 2019_05/src/lib.rs
@@ -5,24 +5,9 @@ mod tests {
     use super::*;
     #[test]
     fn first() {
-        let input = String::from("1,0,0,0,99");
-        assert_eq!(part_1(&input), 2);
-    }
-    #[test]
-    fn second() {
-        let input = String::from("1,1,1,4,99,5,6,0,99");
-        assert_eq!(part_1(&input), 30);
-    }
-    #[test]
-    fn third() {
-        let input = String::from("2,4,4,5,99,0");
-        assert_eq!(part_1(&input), 2);
-    }  
-    #[test]
-    fn fourth() {
-        let input = String::from("2,3,0,3,99");
-        assert_eq!(part_1(&input), 2);
-    }        
+        let input = String::from("1002,4,3,4,33");
+        part_1(&input);
+    }      
 }
 
 pub struct Computer {
@@ -32,21 +17,22 @@ pub struct Computer {
 
 impl Computer {
     pub fn run(&mut self) {
-        self.replace();
-        print!("{:?}\n", self.instructions);
         loop {
-            let next = self.instructions[self.loc];
-            match next {
-                1 => self.op_1(),
-                2 => self.op_2(),
+            let mut ins = self.instructions[self.loc].to_string();
+            while ins.chars().count() < 5{
+                ins = format!("0{}", ins);
+            }
+            let opcode:i32 = ins[3..].parse().unwrap();
+            let p_mode: Vec<bool> = ins[..3].chars().map(|x| x == '0').collect();
+            match opcode {
+                1 => self.op_1(p_mode),
+                2 => self.op_2(p_mode),
+                3 => self.op_3(1),
+                4 => self.op_4(p_mode),
                 99 => break,
-                _ => panic!("Bad instruction match {}\n", next),
+                _ => panic!("Bad instruction match {}\n", opcode),
             };
         }
-    }
-    fn replace(&mut self) {
-        self.update_ins(1, 12);
-        self.update_ins(2, 2);
     }
     fn update_loc(&mut self, shift: usize) {
         self.loc += shift
@@ -54,27 +40,43 @@ impl Computer {
     fn update_ins(&mut self, loc: usize, val: i32) {
         self.instructions[loc] = val
     }
-    fn get_ix(&mut self, i: usize) -> i32 {
+    fn get_ix(&mut self, i: usize) -> i32{
         return self.instructions[i];
     }
-    fn op_1(&mut self) {
-        let i: usize = self.get_ix(self.loc + 3).try_into().unwrap();
-        let a_i: usize = self.get_ix(self.loc + 1).try_into().unwrap();
-        let b_i: usize = self.get_ix(self.loc + 2).try_into().unwrap();
-        let a = self.get_ix(a_i);
-        let b = self.get_ix(b_i);
+    fn get_ix_u(&mut self, i: usize) -> usize{
+        return self.instructions[i].try_into().unwrap();
+    }
+    fn get_val(&mut self, i:usize, pmode: bool)-> i32{
+        if pmode {
+            let pos = self.get_ix_u(i);
+            return self.get_ix(pos);
+        }
+        return self.get_ix(i);
+    }
+    fn op_1(&mut self, p_modes: Vec<bool>) {
+        let i: usize = self.get_ix_u(self.loc + 3);
+        let a = self.get_val(self.loc + 1, p_modes[2]);
+        let b = self.get_val(self.loc + 2, p_modes[1]);
         self.update_ins(i, a + b);
         self.update_loc(4)
     }
-    fn op_2(&mut self) {
-        let i: usize = self.get_ix(self.loc + 3).try_into().unwrap();
-        let a_i: usize = self.get_ix(self.loc + 1).try_into().unwrap();
-        let b_i: usize = self.get_ix(self.loc + 2).try_into().unwrap();
-        let a = self.get_ix(a_i);
-        let b = self.get_ix(b_i);
+    fn op_2(&mut self, p_modes: Vec<bool>) {
+        let i: usize = self.get_ix_u(self.loc + 3);
+        let a = self.get_val(self.loc + 1, p_modes[2]);
+        let b = self.get_val(self.loc + 2, p_modes[1]);
         self.update_ins(i, a * b);
         self.update_loc(4)
     }
+    fn op_3(&mut self, x: i32) {
+        let a: usize = self.get_ix_u(self.loc + 1);
+        self.update_ins(a, x);
+        self.update_loc(2)
+    }
+    fn op_4(&mut self, p_modes: Vec<bool>) {
+        let a:i32  = self.get_val(self.loc + 1, p_modes[2]);
+        print!("op 4: {}\n", a);
+        self.update_loc(2)
+    }       
 }
 
 pub fn part_1(s: &str) -> i32 {
