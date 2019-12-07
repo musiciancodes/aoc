15221647a18dd8f77dba8d8fe36fe03e4d60447f
Yulan Lin <yulan.lin.2014@gmail.com>
2019-12-06 21:02:41 -0800

    built a tree

--- 2019_06/src/lib.rs
+++ 2019_06/src/lib.rs
@@ -1,19 +1,35 @@
+
+extern crate itertools;
+use itertools::Itertools;
 use std::collections::{HashMap};
 
+
 pub fn build_tree(s: &str) -> i32 {
-    // HashSet: parent: [children] (use or_insert) 
-    //
-    let mut orbits: HashMap<&str,  Vec<&str>>;
-    s.lines().for_each(|line| {
-        let v: Vec<&str> = line.split(")").collect();
-        orbits.entry(v[0]).or_insert(Vec::new());
-        //orbits.get(v[0]).unwrap().push(v[1]);
+
+    let mut orbits: Vec<(&str, &str)> = s.lines().map(|line| {
+        line.split(")").next_tuple().unwrap()
+    }).collect();
+
+    let mut tree: HashMap<&str, Vec<&str>> = HashMap::new();
+    orbits.iter_mut().for_each(|orb| {
+        let empty_vec = Vec::new();
+        let children = tree.entry(orb.0).or_insert(empty_vec);
+        children.push(orb.1);
     });
 
+    let mut count = 0;
+    let mut level = 0;
+    print!("{:?}\n", tree);
+    
+    //     let v: Vec<&str> = line.split(")").collect();
+    //     orbits.entry(v[0]).or_insert(Vec::new());
+    //     orbits.get(v[0]).unwrap().push(v[1]);
+    
     return 0;
 }
 
 pub fn part_1(s: &str) -> i32 {
+    build_tree(s);
     return 0;
 }
 
