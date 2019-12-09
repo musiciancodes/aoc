4ab0964a4073fd838a7882a39203cc7a87ce2fd2
Yulan Lin <yulan.lin.2014@gmail.com>
2019-12-08 23:37:09 -0800

    got part 1 working

--- 2019_06/src/lib.rs
+++ 2019_06/src/lib.rs
@@ -1,14 +1,12 @@
-
 extern crate itertools;
 use itertools::Itertools;
-use std::collections::{HashMap};
-
+use std::collections::HashMap;
 
 pub fn build_tree(s: &str) -> i32 {
-
-    let mut orbits: Vec<(&str, &str)> = s.lines().map(|line| {
-        line.split(")").next_tuple().unwrap()
-    }).collect();
+    let mut orbits: Vec<(&str, &str)> = s
+        .lines()
+        .map(|line| line.split(")").next_tuple().unwrap())
+        .collect();
 
     let mut tree: HashMap<&str, Vec<&str>> = HashMap::new();
     orbits.iter_mut().for_each(|orb| {
@@ -18,19 +16,32 @@ pub fn build_tree(s: &str) -> i32 {
     });
 
     let mut count = 0;
-    let mut level = 0;
-    print!("{:?}\n", tree);
-    
-    //     let v: Vec<&str> = line.split(")").collect();
-    //     orbits.entry(v[0]).or_insert(Vec::new());
-    //     orbits.get(v[0]).unwrap().push(v[1]);
-    
-    return 0;
+    let mut level = 1;
+    let mut to_search: Vec<&str> = vec!["COM"];
+    while to_search.len() != 0 {
+        let mut children: Vec<&str> = Vec::new();
+        // add children to a vector
+        to_search.iter().for_each(|p| {
+            print!("p:{}\n", p);
+            let kids = tree.get(p);
+            match kids {
+                Some(kids) => kids.iter().for_each(|c| {
+                    children.push(c);
+                    count += level;
+                }),
+                None => (),
+            };
+        });
+        to_search = children;
+        level += 1;
+    }
+    // traverse the tree:
+
+    return count;
 }
 
 pub fn part_1(s: &str) -> i32 {
-    build_tree(s);
-    return 0;
+    return build_tree(s);
 }
 
 pub fn part_2(s: &str) -> i32 {
